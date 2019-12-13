67a6a1be2e12c28298f30b7523bd4208c03de258
AndyHur1 <hurby@ssfs.org>
2019-12-13 10:15:34 -0500

    array

---
+++ src/TraverseArrays.java
@@ -0,0 +1,49 @@
+public class TraverseArrays {
+    public static int countEven(int[]num){
+
+        int total_num=0;
+        int length = num.length;
+        for (int a=0;a<length;a++){
+
+            if (num[a]%2==0){
+                total_num++;
+            }
+            else{
+                total_num+=0;
+            }
+        }
+        return total_num;
+    }
+
+    public static int[] buildArray(int desiredLength){
+        int [] list = new int[desiredLength];
+        for (int a =0; a<=desiredLength-1;a++){
+            list[a]=a;
+        }
+        return list;
+
+    }
+    public static boolean sum28(int[] numbers){
+        int times = numbers.length;
+        int total_u = 0;
+        for (int a=0;a<times;a++){
+            if (numbers[a]==2){
+                total_u+=numbers[a];
+            }else{
+                total_u+=0;
+            }
+
+        }
+        if (total_u==8){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    public static int[] zeroMax(int[] original){
+
+    }
+
+
+}
