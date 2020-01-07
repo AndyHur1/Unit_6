e9e0144bbee228193416a2a5abfa8257ce323001
AndyHur1 <hurby@ssfs.org>
2020-01-07 09:00:19 -0500

    finished ArrayAlgorithms

--- src/ArrayAlgorithms.java
+++ src/ArrayAlgorithms.java
@@ -27,20 +27,50 @@ public class ArrayAlgorithms {
 
     public static boolean hasDupes (int[] list){
 
-        boolean result = true;
-        for (int a=0;a<=list.length;a++){
-            for (int b= a+1;b<=list.length;a++){
+
+        for (int a=0;a<=list.length-1;a++){
+            for (int b= a+1;b<=list.length-1;b++){
                 if (list[a]==list[b]){
-                    result = true;
-                }else{
-                    result=false;
+                    return true;
+
                 }
             }
 
 
+
         }
 
-        return result;
+        return false;
     }
+    public static int countEvens (int[] arr){
+        int length = arr.length;
+        int times = 0;
+        for (int a=0; a<=length-1;a++){
+            if (arr[a]%2==0){
+                times+=1;
+            }else
+                times+=0;
+
+        }
+        return times;
+    }
+    public static int[] leftShift(int[] nums, int shift){
+        int shifted_array []= new int [nums.length+shift];
+        for (int a=0; a<=nums.length-1;a++){
+            shifted_array[a]=nums[a];
+        }
+        return shifted_array;
+    }
+    public static String reverseArray(int[] nums){
+        int temp_num;
+        for (int a=0;a<nums.length/2;a++){
+            temp_num = nums[a];
+            nums[a]=nums[nums.length-a-1];
+            nums[nums.length-a-1]=temp_num;
+        }
+
+      return printArray(nums);
+    }
+
 
 }
